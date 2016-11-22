bbe2cf7752044f1dda69acda275a05015c3ae1f3
Justin Gagne <justin.gagne@montserrat.edu>
2016-11-22 15:08:04 -0500

    Test UTM code tracking
    Hidden fields and jQuery magic?

--- gym-enhancements/events/production/js/functions.js
+++ gym-enhancements/events/production/js/functions.js
@@ -8,3 +8,25 @@ $("#event-form").validate({
     }
   }
 });
+
+// utm capture
+
+getParameterByName = function(name) {
+query = window.location.search.toString();
+name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+var regexS = "[\\?&]" + name + "=([^&#]*)";
+var regex = new RegExp(regexS);
+results = regex.exec(query);
+if (results == null) return "";
+else return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+$(document).ready(function() {
+var $utmSource = $('[name=utm_source]'),
+$utmMedium = $('[name=utm_medium]'),
+$utmContent = $('[name=utm_content]'),
+$utmCampaign = $('[name=utm_campaign]');
+$utmSource.val(getParameterByName('utm_source'));
+$utmMedium.val(getParameterByName('utm_medium'));
+$utmContent.val(getParameterByName('utm_content'));
+$utmCampaign.val(getParameterByName('utm_campaign'));
+});
